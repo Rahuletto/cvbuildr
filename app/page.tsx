776919cd8de47cab1972f553d9bc304ea9ffd71e
Rahuletto <rahulmarban@gmail.com>
2024-12-01 17:33:58 +0530

    style: animations

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
       <section className="flex relative pt-5 gap-3 opacity-0 animate-fadeIn xl:pt-0 justify-between xl:flex-row flex-col xl:items-end items-center lg:px-32 md:px-16 px-4 h-[80vh]">
         <div className="flex gap-5 flex-col h-full justify-center">
-          <img src="/mascot.svg" className="relative lg:w-12 w-8 -mt-16 lg:-ml-8 -ml-4 -rotate-6" />
+          <img style={{ animationDelay: "1.2s" }} src="/mascot.svg" className="relative lg:w-12 w-8 -mt-16 lg:-ml-8 -ml-4 -rotate-6 animate-fadeIn opacity-0" />
           <h2 style={{ animationDelay: "0.5s" }} className="text-4xl lg:-mt-4 -mt-6 xl:text-7xl font-bold animate-fadeIn opacity-0">Build your resume<br />in <span className="underline">no-time</span></h2>
           <p style={{ animationDelay: "1s" }} className="lg:text-lg text-base lg:mb-4 font-semibold opacity-0 animate-opacityFade">Build an ATS-compliant resume to showcase what<br />you&apos;ve done so far</p>
           <div className="flex gap-6 font-semibold items-center">
@@ -25,7 +25,7 @@ export default function Home() {
             </Link>
 
             <Link
-              href="/analyze"
+              href="/analyse"
               style={{ animationDelay: "1.5s" }}
               className="z-10 animate-fadeIn flex items-center justify-center opacity-0 lg:text-xl text-base px-4 py-2 rounded-xl font-semibold text-black border-2 border-black bg-white overflow-hidden transition-all duration-300 hover:shadow-hover active:shadow-active"
             >
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
         </div>
         <div className="xl:relative xl:w-[50%] w-full h-full flex items-end xl:mb-10 justify-center xl:scale-110">
-          <div className="z-50 absolute bottom-64 right-48">
+          <div style={{ animationDelay: "0.8s" }} className="z-50 absolute bottom-[-20px] right-[-100px] animate-cursor">
             <img src="/cursor.svg" className="flex flex-col" />
             <span className="px-2 py-0.5 text-sm font-semibold bg-black ml-6 relative -top-2 -rotate-3 flex text-white rounded-md">Marban</span>
           </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,9 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
       animation: {
-        fadeIn: 'fadeIn 0.25s ease-out forwards', // Define the fadeIn animation
-        opacityFade: 'opacityFade 0.25s ease-out forwards', // Define the fadeIn animation
+        fadeIn: 'fadeIn 0.25s ease-out forwards',
+        opacityFade: 'opacityFade 0.25s ease-out forwards',
+        cursor: 'cursor 1.7s cubic-bezier(0.19, 1, 0.22, 1) forwards, hovering 6s infinite',
       },
       keyframes: {
         fadeIn: {
@@ -25,6 +26,28 @@ const config: Config = {
           '0%': { opacity: '0' },
           '100%': { opacity: '0.5' },
         },
+        cursor: {
+          "0%": {
+            bottom: "-20px",
+            right: "-100px"
+          },
+          "50%": {
+            bottom: "90px",
+            right: "240px",
+            transform: "translateX(-50%) translateY(-50%)"
+          },
+          "100%": {
+            bottom: "256px",
+            right: "192px",
+            transform: "translateX(0) translateY(0)"
+          }
+        },
+        hovering: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '25%': { transform: 'translateX(3px) translateY(-2px)' },
+          '50%': { transform: 'translateX(2px) translateY(-4px)' },
+          '75%': { transform: 'translateX(3px) translateY(2px)' },
+        }
       },
     },
   },
