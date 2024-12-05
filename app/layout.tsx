7ac57b4dedd068382b76a6e57424292e3bcda1b7
Rahuletto <rahulmarban@gmail.com>
2024-12-05 13:42:18 +0530

    style: font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import Script from "next/script";
 import { ReactNode } from "react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "cvbuildr",
@@ -29,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
         >
           <Script
             id="razorpay-checkout-js"
